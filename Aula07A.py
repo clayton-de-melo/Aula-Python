d928b4ec5ad86190a9cbb2391dfdc808c81c1e3b
Clayton <159891137+clayton-de-melo@users.noreply.github.com>
2025-02-19 11:23:35 -0300

    aula 7

---
+++ Aula07A.py
@@ -0,0 +1,12 @@
+n1 = int(input('Digite um Valor:'))
+n2 = int(input('Digite outro valor:'))
+
+#print('A Soma vale {}!'.format(n1+n2))
+
+s = n1 = n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma vale {},\na multiplicação é {}\ne a divisão é {:.3f}\n'.format(s,m,d), end='>>>')
+print(' E a Divisão inteira {} e potencia {}'.format(di,e), end='')
